da5b8db815a48c54c411993e875dd3b851fa8e23
逆水行舟 <524480797@qq.com>
2025-05-26 23:39:15 +0800

    1

---
+++ day17_Llamaindex快速构建RAG/code/modeldownload.py
@@ -0,0 +1,3 @@
+#模型下载
+from modelscope import snapshot_download
+model_dir = snapshot_download('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2',cache_dir='/root/AI-WSL/models/embedding_model')
